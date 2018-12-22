c61927754aa6d6111364ebba85ed9c00196a35db
pmtavares <pedrotavares41@gmail.com>
2018-12-22 15:27:58 +0000

    Class Beans for Staff Added

---
+++ src/Beans/StaffBeans.java
@@ -0,0 +1,51 @@
+package Beans;
+
+/**
+ *
+ * @author Pedro
+ */
+public class StaffBeans {
+    private int code;
+    private String name;
+    private String function;
+    private String dateReg;
+    
+    public StaffBeans()
+    {
+        
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getFunction() {
+        return function;
+    }
+
+    public void setFunction(String function) {
+        this.function = function;
+    }
+
+    public String getDateReg() {
+        return dateReg;
+    }
+
+    public void setDateReg(String dateReg) {
+        this.dateReg = dateReg;
+    }
+    
+    
+}
